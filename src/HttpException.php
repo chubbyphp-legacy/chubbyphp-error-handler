32b241bb7b61b8e4f63fb99e6836c32dc1eb7349
Dominik Zogg <dominik.zogg@gmail.com>
2016-10-03 20:42:07 +0000

    Revert "HttpException does not need the request and response"
    This reverts commit abe56090004063484ff2de488532a5579104b55b.

--- src/HttpException.php
+++ src/HttpException.php
@@ -2,16 +2,51 @@
 
 namespace Chubbyphp\ErrorHandler;
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
 final class HttpException extends \RuntimeException
 {
     /**
-     * @param int    $status
-     * @param string $message
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @var Response
+     */
+    private $response;
+
+    /**
+     * @param Request  $request
+     * @param Response $response
+     * @param int      $status
+     * @param string   $message
      *
      * @return HttpException
      */
-    public static function create(int $status, string $message): self
+    public static function create(Request $request, Response $response, int $status, string $message): self
     {
-        return new self($message, $status);
+        $exception = new self($message, $status);
+        $exception->request = $request;
+        $exception->response = $response;
+
+        return $exception;
+    }
+
+    /**
+     * @return Request
+     */
+    public function getRequest(): Request
+    {
+        return $this->request;
+    }
+
+    /**
+     * @return Response
+     */
+    public function getResponse(): Response
+    {
+        return $this->response;
     }
 }
